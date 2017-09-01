ffb6d9d763b6614ac403ca185673d439dabb86aa
Patience Mpofu <patience.mpofu@jembi.org>
2017-09-01 15:18:08 +0200

    RAD-26 Create publish and subscribe UIs This change creates custom UIs for devices to publish and subscribe to topics

---
+++ paho.mqtt.android.example/src/main/java/paho/mqtt/java/example/PublishActivity.java
@@ -0,0 +1,10 @@
+package paho.mqtt.java.example;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Jembi Health Systems 2017/09/01.
+ */
+
+public class PublishActivity extends AppCompatActivity {
+}
---
+++ paho.mqtt.android.example/src/main/java/paho/mqtt/java/example/SubscribeActivity.java
@@ -0,0 +1,10 @@
+package paho.mqtt.java.example;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Jembi Health Systems 2017
+ */
+
+public class SubscribeActivity extends AppCompatActivity {
+}
